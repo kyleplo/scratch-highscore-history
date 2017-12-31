45e592e93f29fdfcf09c48b6b35f4b17062a5fd4
kyleplo <31634240+kyleplo@users.noreply.github.com>
2017-12-31 12:50:23 -0500

    Add error messages

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function trimProjectUrl(url){if(isNaN(url.charAt(url.lastIndexOf("/") - 1))){return url.slice(33)}else{return url.slice(33,url.lastIndexOf("/"))}}
 function load(id){
-fetch("https://cors-everywhere.herokuapp.com/clouddata.scratch.mit.edu/logs?projectid=" + id + "&limit=1000&offset=0").then(function (r){return r.json()}).then(function (j){
+  document.getElementById("loading").removeAttribute("hidden");
+  document.getElementById("loading").innerHTML = "Loading...";
+fetch("https://cors-everywhere.herokuapp.com/clouddata.scratch.mit.edu/logs?projectid=" + id + "&limit=1000&offset=0").then(function (r){if(r.ok){return r.json()}else{document.getElementById("loading").innerHTML = "Error: Reload and try again"}}).then(function (j){
 console.log("Fetched: " + j);
 var scores = [];
 var lastUser = "";
@@ -12,8 +14,8 @@ var scoreHtml = "";
 for(var i = 0;i < scores.length;i++){
 scoreHtml += "<li><a href='https://scratch.mit.edu/users/" + scores[i].user + "'>@" + scores[i].user + "</a>: " + scores[i].score + " <span>" + timeago().format(scores[i].date) + "</span></li>";
 }
-document.getElementById("scores").innerHTML = scoreHtml;
-})
+document.getElementById("scores").innerHTML = scoreHtml;document.getElementById("loading").setAttribute("hidden","hidden");
+}).catch(function (r){document.getElementById("loading").innerHTML = "Error: " + r.message + "<br>Reload and try again";});
 }
 window.addEventListener("load",function (){new Clipboard('#copy');
 if(location.hash.length > 1){load(location.hash.slice(1))}else{document.getElementById("learn").setAttribute("hidden","hidden");document.getElementById("about").removeAttribute("hidden")}
